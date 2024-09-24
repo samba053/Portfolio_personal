ceddade6a8ac243c3fd172bfdae37e5e56bc61e8
samba053 <sambathodeti@gmail.com>
2024-09-24 20:39:23 +0530

    created repo

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text",{
+    Strings: ["FrontEnd Develepor", "Youtube Develepor","Web Develepor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeley: 1000,
+    loop:true       
+});
